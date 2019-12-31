f89d9b1aa237637371b7e4a61d83a3bf248fbcaf
yanxiaosong0902 <xiaosongkeith@163.com>
2019-12-31 15:29:49 +0800

    v1.0

--- roll.py
+++ roll.py
@@ -19,45 +19,63 @@ def main():
     'Junc': {
       'name': 'Junc',
       'score': 0,
-      'win': False
+      'win': False,
+      'gold': 0,
+      'balance': 0
     },
     'qinbao': {
       'name': '沁宝',
       'score': 0,
-      'win': False
+      'win': False,
+      'gold': 0,
+      'balance': 0
     },
     'jianming': {
       'name': '小弦弦',
       'score': 0,
-      'win': False
+      'win': False,
+      'gold': 0,
+      'balance': 0
     },
     'dapeng': {
       'name': '雨魔',
       'score': 0,
-      'win': False
+      'win': False,
+      'gold': 0,
+      'balance': 0
     },
     'guli': {
       'name': '十字军打击',
       'score': 0,
-      'win': False
+      'win': False,
+      'gold': 0,
+      'balance': 0
     },
     'jiangqi': {
       'name': '吹胡子咬耳朵',
       'score': 0,
-      'win': False
+      'win': False,
+      'gold': 0,
+      'balance': 0
     }
   }
-  gold = argsParse(users)
-  winUsers = []
+  
+  init = argsParse(users)
+  
+  loop(init)
 
-  for v in users.items():
-    if v[1]['win'] == True:
-      winUsers.append(v[1])
-
-  print('本次有 %d 名玩家获胜！' % len(winUsers))
-
-  for v in winUsers:
-    print('%s 分得 %d 金' % ( v['name'], gold / len(winUsers)))
+def loop(init):
+  time = 0
+  min, max, win, gold, times = init
+  stake = int(gold / times) * len(users)
+  while time < times:
+    isGo = input("载入完成，请选择是否继续（y/n）？")
+    if isGo == 'y' or isGo == 'yes':
+      print("go!")
+      roll(min, max, win, stake)
+    else:
+      break
+  print("欢迎下次再来！")
 
 def isWin(score, win):
   if win == 'odd':
@@ -67,24 +85,47 @@ def isWin(score, win):
 
 def argsParse(users):
   parser = apa.ArgumentParser()
-  parser.add_argument("win", help="win method")
+  parser.add_argument("win", "-w", "--win", help="win method")
+  parser.add_argument("times", "-t", "--times", help="times", default="10")
   parser.add_argument("-min", "--min", help="min value", default=1)
+  parser.add_argument("-g", "--gold", help="gold count", default=200)
   parser.add_argument("-max", "--max", help="max value", default=100)
-  parser.add_argument("-g", "--gold", help="gold count", default=100)
   args = parser.parse_args()
 
   min = args.min
   max = args.max
   win = args.win
+  time = args.times
+  gold = int(args.gold)
+  for v in users.values():
+    v['gold'] = gold
+    v['balance'] = 0
 
+  return (min, max, win, gold, times)
+
+def roll(min, max, win, gold):
   for v in users.values():
     score = round(random.random() * 100)
     v['score'] = score
     v['win'] = isWin(score, win)
     date = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
     print("%s %s 掷出 %d (%s - %s)" % (date, v['name'], score, min, max))
+
+  winUsers = []
+
+  for k, v in users.items():
+    if v['win'] == True:
+      winUsers.append(v)
+
+  averageGold = gold / len(winUsers)
   
-  return int(args.gold)
+  for v in winUsers:
+      v['balance'] += averageGold
+
+  print('本次有 %d 名玩家获胜！' % len(winUsers))
+
+  for v in winUsers:
+    print('%s 分得 %d 金' % ( v['name'], averageGold)
 
 if __name__ == "__main__":
   main()
